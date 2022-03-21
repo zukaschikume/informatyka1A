c62921af1f33a93702a626afa03f593ecbf5da51
Bartosz Wójcik <wojcikbartosz.06@gmail.com>
2022-03-21 23:40:26 +0100

    liczby pierwsze

---
+++ liczby pierwsze/liczby pierwsze.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int p;
+    int lp;
+    int d;
+    lp = 0;
+    bool flaga;
+    cout << "ile liczb pierwszych";
+    cin >> n;
+    p = 2;
+    for (lp = 0; lp < n;) {
+        flaga = false;
+        for (d = 2; d < p; d += 1) {
+            if (p % d == 0) {
+                flaga = true;
+            }
+        }
+        if (flaga == false) {
+            cout << p << ",";
+            lp += 1;
+            p += 1;
+        }
+        else { p += 1; }
+    }
+}
+
